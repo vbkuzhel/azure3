c42d166332cc9f7eeed8d457bb70e8c6b8092b3b
Volodymyr Kuzhel <kuzhel@gmail.com>
2018-03-13 18:04:34 +0200

    tested create network function

--- automationServices.ps1
+++ automationServices.ps1
@@ -13,7 +13,7 @@ Perform these preliminary steps that allow you to proceed with Automation accoun
 1.done.	Log in to portal.azure.com with your test (non-production) Microsoft account. (Subscribing for Azure trial is a course prerequisite).
 2.done.	Create a resource group “MyAutomationHelper” in region “West Central US” (you can choose any region that allows you to maintain Automation account in your subscription.) 
 3.done.	Create a resource group “MyVMs” in region “West Central US” (you can choose any region that you prefer to maintain virtual machines in your subscription.)
-4.	Locate azure virtual networks tab in Azure preview portal and create a virtual network. Set the name of virtual network to 
+4.done.	Locate azure virtual networks tab in Azure preview portal and create a virtual network. Set the name of virtual network to 
 “MyVnet”.  Target it to resource group “MyVMs” and region “West Central US” (or your preferred region based on item 3.) 
 Assign a private address space to Vnet, for example, 172.16.0.0/16. Create one or more subnets in your Vnet and assign it an address range out of VNet space, for example,
  172.16.1.0/24, 172.16.2.0/24, and so on. Name subnets “MySubnet1”, “MySubnet2” etc.
@@ -26,9 +26,9 @@ $RGAutomationName="MyAutomationHelper"
 $RGVnet
 $RGAutomation
 $Subnet1Name="MySubnet1"
-$Subnet1Adress="172.16.1.0/24"
+$Subnet1Address="172.16.1.0/24"
 $Subnet2Name="MySubnet2"
-$Subnet2Adress="172.16.2.0/24"
+$Subnet2Address="172.16.2.0/24"
 $VnetName="MyVnet"
 $VnetAddress="172.16.0.0/16"
 $Vnet
@@ -51,7 +51,7 @@ function createNetworks{
     Write-Host "crete Networks"
     $subnets=@()
     $subnets+= New-AzureRmVirtualNetworkSubnetConfig -Name $Subnet1Name -AddressPrefix $Subnet1Address
-    $subnets+= New-AzureRmVirtualNetworkSubnetConfig -Name $Subnet2Name -AddressPrefix $Subnet1Address
+    $subnets+= New-AzureRmVirtualNetworkSubnetConfig -Name $Subnet2Name -AddressPrefix $Subnet2Address
     $Vnet= New-AzureRMVirtualNetwork -Name $VnetName -ResourceGroupName $RGVnetName -location $location -addressprefix $VnetAddress -subnet $subnets
 }
 function deleteNetworks{
